24554bc3b464e11a353764fe3d831efe0fb44ad7
TechnoDevGamerLabs <technodevgamerlabs@gmail.com>
2024-06-01 23:46:57 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const imageContainer = document.querySelector(".image-container");
+
+// Aquí vamos a añadir el código para cargar las imágenes de la carpeta
+// y crear los elementos de imagen.
+
+// Ejemplo:
+// imageContainer.innerHTML += `<div class="image-item"><img src="path/to/image.jpg" alt="Animal"></div>`; 
+
+// Y después, el código para añadir el evento de clic para redirigir a la página del animal.
